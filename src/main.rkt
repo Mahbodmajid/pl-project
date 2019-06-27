886dd42cb8615dfc45358cfb0ef62dd23d652865
mahbodmajid <mahbod.majid@gmail.com>
2019-06-27 14:26:18 +0430

    FIX last commit

--- src/main.rkt
+++ src/main.rkt
@@ -2,64 +2,18 @@
 
 (require (lib "eopl.ss" "eopl"))
 
-(define program-url "my_sample.txt")
+(define program-url "sample/sample_input.txt")
 
-
-(define remove-all
-  (lambda (ele lst)
-    (cond
-      ((null? lst) '())           ; if the list is empty then we're done
-      ((not (pair? (car lst)))    ; if the first element is an atom
-       (if (equal? (car lst) ele) ; check if it's the one we're looking for
-           (remove-all ele (cdr lst)) ; if so we skip over it, eliminating it
-           (cons (car lst) (remove-all ele (cdr lst))))) ; otherwise we add it
-      (else (cons (remove-all ele (car lst))      ; else advance recursion
-                  (remove-all ele (cdr lst))))))) ; over both `car` and `cdr`
-
-
-(define program-lines
-  (remove-all ""
-              (map string-normalize-spaces
-                   (file->lines program-url #:mode'text))))
-
-(define extract-setup-commands
-  (lambda (lines)
-    (let ([store '(() ()) ])
-      (extract-setup-commands-helping lines 'doesntmatter store))))
-
-; ((setup) (commands))
-
-
-(define extract-setup-commands-helping
-  (lambda (lines dir result)
-    (if (equal? lines '())
-        result
-        (let ([head (car lines)]
-              [tail (cdr lines)]
-              [setup (car result)]
-              [commands (cadr result)])
-          (cond
-            [(equal? head "setup") (extract-setup-commands-helping tail 's result)]
-            [(equal? head "commands") (extract-setup-commands-helping tail 'c result)]
-            [(equal? dir 's) (extract-setup-commands-helping tail 's
-                                                             (list (append setup (list head)) commands))]
-            [(equal? dir 'c) (extract-setup-commands-helping tail 'c
-                                                             (list setup (append commands (list head))))]
-            [else (error "something is wrong")])))))
+(require "program-to-setup-and-command.rkt")
 
 (define commands '())
 (define setup '())
 
-(define dummy
-  (match (extract-setup-commands program-lines)
-    [(list lsetup lcommands)
-     (begin
-     (set! commands lcommands)
-     (set! setup lsetup))
-     ]))
+(set-commands-setup! commands setup)
 
-setup
 commands
+setup
+
 
 (define-datatype account-type account-type?
   (a-account-type (account-id number?)
